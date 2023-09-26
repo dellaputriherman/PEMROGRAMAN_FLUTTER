cf8009036095fc87bc213d087dfc8166e6d5f360
DellaPutriHerman <127371139+dellaputriherman@users.noreply.github.com>
2023-09-26 15:06:40 +0700

    Update FLUTTER 07 List View

---
+++ listview/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final List<Color> myColor = [
+    Colors.purple,
+    Colors.red,
+    Colors.blue,
+    Colors.brown,
+  ];
+
+  final List<Widget> myList = List.generate(
+    100,
+    (index) => Text(
+      "${index + 1}",
+      style: TextStyle(
+        fontSize: 20 + double.parse(index.toString()),
+      ),
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("List View"),
+        ),
+        body: ListView(
+          children: myList,
+        ),
+      ),
+    );
+  }
+}
