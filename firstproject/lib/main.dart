6afee823beed92d5af47d1f149ce2f2e7ed8eb59
DellaPutriHerman <127371139+dellaputriherman@users.noreply.github.com>
2023-09-26 12:42:54 +0700

    Update FLUTTER 05 Text Widget

--- firstproject/lib/main.dart
+++ firstproject/lib/main.dart
@@ -10,12 +10,27 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.pink,
-        body: Center(
-          child: Text("Hallo"),
-        ),
         appBar: AppBar(
           title: Text("Della Putri Herman"),
+        ),
+        body: Center(
+          child: Text(
+            "Hallo Nama Saya Della Putri Herman dari Kelas TK 2A Prodi Teknik Komputer Mata Kuliah Pemograman Mobile Lanjut",
+            maxLines: 5,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              backgroundColor: Colors.pinkAccent,
+              color: Colors.black,
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Stick',
+              decorationStyle: TextDecorationStyle.dotted,
+              decoration: TextDecoration.lineThrough,
+              decorationColor: Colors.white,
+              decorationThickness: 5,
+            ),
+          ),
         ),
       ),
     );
