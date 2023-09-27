eda8ed4b9144fba2eb4fdad6be8a425b91c9bf5a
DellaPutriHerman <127371139+dellaputriherman@users.noreply.github.com>
2023-09-27 22:13:31 +0700

    Upload FLUTTER 10 Extract Widget

---
+++ extract_widget/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Extract Widget"),
+        ),
+        body: ListView(
+          children: [
+            ChatItem(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ChatItem extends StatelessWidget {
+  const ChatItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(),
+      title: Text("Name"),
+      subtitle: Text("Subtitle"),
+      trailing: Text("09.00 PM"),
+    );
+  }
+}
