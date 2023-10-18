27dbd1d91fdfc00b52006b39dc1b19987bff6268
DellaPutriHerman <127371139+dellaputriherman@users.noreply.github.com>
2023-10-19 00:34:17 +0700

    Update FLUTTER 15 TabBar Widget

--- tabbar_widget/lib/main.dart
+++ tabbar_widget/lib/main.dart
@@ -12,8 +12,10 @@ class MyApp extends StatelessWidget {
     ),
     Tab(
       icon: Icon(Icons.abc_outlined),
+      text: "Tab 2",
     ),
     Tab(
+      icon: Icon(Icons.accessibility_rounded),
       text: "Tab 3",
     ),
   ];
@@ -21,19 +23,56 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: DefaultTabController(
+        initialIndex: 2,
         length: myTab.length,
         child: Scaffold(
           appBar: AppBar(
             title: Text("My Apps"),
             bottom: TabBar(
-              indicatorColor: Colors.deepPurple,
-              indicatorWeight: 5,
-              indicatorPadding: EdgeInsets.all(2),
-              indicator: BoxDecoration(
-                color: Colors.pinkAccent,
+              labelColor: Colors.black,
+              unselectedLabelColor: Colors.white,
+              labelStyle: TextStyle(
+                fontWeight: FontWeight.bold,
               ),
+              unselectedLabelStyle: TextStyle(
+                fontWeight: FontWeight.normal,
+              ),
+              // indicatorColor: Colors.deepPurple,
+              // indicatorWeight: 5,
+              // indicatorPadding: EdgeInsets.all(2),
+              indicator: BoxDecoration(
+                  color: Colors.pinkAccent,
+                  //borderRadius: BorderRadius.circular(50),
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Colors.black,
+                      width: 5,
+                    ),
+                  )),
               tabs: myTab,
             ),
+          ),
+          body: TabBarView(
+            children: [
+              Center(
+                child: Text(
+                  "Tab 1",
+                  style: TextStyle(fontSize: 40),
+                ),
+              ),
+              Center(
+                child: Text(
+                  "Tab 2",
+                  style: TextStyle(fontSize: 40),
+                ),
+              ),
+              Center(
+                child: Text(
+                  "Tab 3",
+                  style: TextStyle(fontSize: 40),
+                ),
+              ),
+            ],
           ),
         ),
       ),
