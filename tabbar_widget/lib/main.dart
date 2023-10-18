e71b08e972d9f22c588e189a837419719c5ea3b5
DellaPutriHerman <127371139+dellaputriherman@users.noreply.github.com>
2023-10-19 00:16:26 +0700

    Update FLUTTER 15 TabBar Widget

--- tabbar_widget/lib/main.dart
+++ tabbar_widget/lib/main.dart
@@ -5,15 +5,35 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  List<Tab> myTab = [
+    Tab(
+      text: "Tab 1",
+      icon: Icon(Icons.add_a_photo),
+    ),
+    Tab(
+      icon: Icon(Icons.abc_outlined),
+    ),
+    Tab(
+      text: "Tab 3",
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("My Apps"),
-          bottom: PreferredSize(
-            preferredSize: Size.fromHeight(100),
-            child: Text("Bottom"),
+      home: DefaultTabController(
+        length: myTab.length,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("My Apps"),
+            bottom: TabBar(
+              indicatorColor: Colors.deepPurple,
+              indicatorWeight: 5,
+              indicatorPadding: EdgeInsets.all(2),
+              indicator: BoxDecoration(
+                color: Colors.pinkAccent,
+              ),
+              tabs: myTab,
+            ),
           ),
         ),
       ),
