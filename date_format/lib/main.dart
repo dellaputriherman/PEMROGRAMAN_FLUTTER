5c47b30b111d050ee2b690ee0c03f3ef121127dc
DellaPutriHerman <127371139+dellaputriherman@users.noreply.github.com>
2023-10-18 22:02:59 +0700

    Upload FLUTTER 13 Date Format

---
+++ date_format/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Date Format"),
+        ),
+        body: Center(
+          child: Text(
+            "Tanggal",
+            style: TextStyle(fontSize: 25),
+          ),
+        ),
+      ),
+    );
+  }
+}
