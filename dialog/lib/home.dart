4db41780096d7456fb010cd2e1bdcfa80c215eb5
DellaPutriHerman <127371139+dellaputriherman@users.noreply.github.com>
2023-10-19 03:07:16 +0700

    Update FLUTTER 20 Dialog

--- dialog/lib/home.dart
+++ dialog/lib/home.dart
@@ -1,27 +1,97 @@
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatelessWidget {
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  String data = "Belum ada input";
+
+  void _showDeleteConfirmationDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return DeleteConfirmationDialog(
+          onConfirm: () {
+            debugPrint("Klik Yes");
+            setState(() {
+              data = "TRUE";
+            });
+          },
+        );
+      },
+    ).then((value) {
+      debugPrint(value.toString());
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Dialog"),
       ),
+      body: Center(
+        child: Text(
+          data,
+          style: TextStyle(
+            fontSize: 35,
+          ),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          print("Telah diklik");
-          showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                title: Text("CONFIRM"),
-              );
-            },
-          );
+          debugPrint("Telah diklik");
+          _showDeleteConfirmationDialog(context);
         },
         child: Icon(Icons.delete),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
+
+class DeleteConfirmationDialog extends StatelessWidget {
+  final VoidCallback onConfirm;
+
+  DeleteConfirmationDialog({required this.onConfirm});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("CONFIRM"),
+      content: Text("Are you sure to delete this item?"),
+      actions: [
+        TextButton(
+          onPressed: () {
+            debugPrint("Klik No");
+            Navigator.of(context).pop(false);
+          },
+          child: Text("No"),
+        ),
+        TextButton(
+          onPressed: () {
+            onConfirm();
+            Navigator.of(context).pop(true);
+          },
+          child: Text("Yes"),
+        ),
+      ],
     );
   }
 }
