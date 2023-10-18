518e5c89d673029c0094dd718e7fc31deafee497
DellaPutriHerman <127371139+dellaputriherman@users.noreply.github.com>
2023-10-19 02:23:18 +0700

    Upload FLUTTER 19 Grid View

---
+++ grid_view/lib/main.dart
@@ -0,0 +1,40 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final List<Container> myList = List.generate(50, (index) {
+    return Container(
+      height: 50,
+      width: 150,
+      color: Color.fromARGB(
+        255,
+        Random().nextInt(256),
+        Random().nextInt(256),
+        Random().nextInt(256),
+      ),
+    );
+  });
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Grid View"),
+        ),
+        body: GridView(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 10,
+          ),
+          children: myList,
+        ),
+      ),
+    );
+  }
+}
