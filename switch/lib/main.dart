8d0a0cfc2d4924104a61ed97e202fedeb2c629b8
DellaPutriHerman <127371139+dellaputriherman@users.noreply.github.com>
2023-10-26 02:10:36 +0700

    Update FLUTTER 25 Switch

--- switch/lib/main.dart
+++ switch/lib/main.dart
@@ -28,15 +28,30 @@ class _MyWidgetState extends State<MyHome> {
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Switch(
-              value: statusSwitch,
-              onChanged: (value) {
-                setState(() {
-                  statusSwitch = !statusSwitch;
-                });
-                print(statusSwitch);
-              },
+            Container(
+              color: Colors.grey,
+              //height: 10,
+              //width: 100,
+              child: Switch.adaptive(
+                activeColor: Colors.deepPurple,
+                inactiveTrackColor: Colors.red,
+                activeTrackColor: Colors.green,
+                inactiveThumbColor: Colors.pinkAccent,
+                activeThumbImage: AssetImage("images/true.jpeg"),
+                inactiveThumbImage: AssetImage("images/false.png"),
+                value: statusSwitch,
+                onChanged: (value) {
+                  setState(() {
+                    statusSwitch = !statusSwitch;
+                  });
+                  print(statusSwitch);
+                },
+              ),
+            ),
+            SizedBox(
+              height: 25,
             ),
             Text(
               (statusSwitch == true) ? "Switch On" : "Switch Off",
