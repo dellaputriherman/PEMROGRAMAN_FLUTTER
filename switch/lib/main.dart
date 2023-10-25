669101d41ab487ff57d2e475ba4d0fb1cab59ea1
DellaPutriHerman <127371139+dellaputriherman@users.noreply.github.com>
2023-10-26 01:34:09 +0700

    Upload FLUTTER 25 Switch

---
+++ switch/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatefulWidget {
+  @override
+  State createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyHome> {
+  bool statusSwitch = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Switch"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Switch(
+              value: statusSwitch,
+              onChanged: (value) {
+                setState(() {
+                  statusSwitch = !statusSwitch;
+                });
+                print(statusSwitch);
+              },
+            ),
+            if (statusSwitch == true)
+              Text(
+                "Switch On",
+                style: TextStyle(
+                  fontSize: 35,
+                ),
+              )
+            else
+              Text(
+                "Switch Off",
+                style: TextStyle(
+                  fontSize: 35,
+                ),
+              )
+          ],
+        ),
+      ),
+    );
+  }
+}
