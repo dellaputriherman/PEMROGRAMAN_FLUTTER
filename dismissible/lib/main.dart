811d19330919ff497db2d7aee47e5389859e40ea
DellaPutriHerman <127371139+dellaputriherman@users.noreply.github.com>
2023-10-25 15:10:26 +0700

    Update FLUTTER 21 Dismissible

--- dismissible/lib/main.dart
+++ dismissible/lib/main.dart
@@ -26,10 +26,36 @@ class MyHomePage extends StatelessWidget {
         padding: EdgeInsets.all(8),
         itemCount: 100,
         itemBuilder: (context, index) {
-          return ListTile(
-            leading: CircleAvatar(),
-            title: Text(faker.person.name()),
-            subtitle: Text(faker.lorem.sentence()),
+          return Dismissible(
+            onDismissed: (direction) {
+              print("DISMISSBLE");
+              print("direction");
+              if (direction == DismissDirection.endToStart) {
+                print("END TO START");
+              } else {
+                print("START TO END");
+              }
+            },
+            key: Key(index.toString()),
+            //direction: DismissDirection.endToStart,
+            background: Container(
+              color: Colors.redAccent,
+              child: Icon(
+                Icons.delete,
+                size: 25,
+              ),
+              alignment: Alignment.centerRight,
+              padding: EdgeInsets.only(right: 10),
+            ),
+            child: ListTile(
+              leading: CircleAvatar(
+                child: Center(
+                  child: Text("${index + 1}"),
+                ),
+              ),
+              title: Text(faker.person.name()),
+              subtitle: Text(faker.lorem.sentence()),
+            ),
           );
         },
       ),
